9914906069998f76521ab298d70713714b3e0e9c
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2023-02-01 17:13:51 +0800

    Add files via upload

--- C++ Examples/01_Quick guide/HelloWorld.cpp
+++ C++ Examples/01_Quick guide/HelloWorld.cpp
@@ -1,1 +1,24 @@
+#include "pch.h"
+using namespace Spire::Doc;
 
+int main(){
+	wstring output_path = OUTPUTPATH;
+	wstring outputFile = output_path + L"HelloWorld.docx";
+
+	//Create word document
+	Document* document = new Document();
+
+	//Create a new section
+	Section* section = document->AddSection();
+
+	//Create a new paragraph
+	Paragraph* paragraph = section->AddParagraph();
+
+	//Append Text
+	paragraph->AppendText(L"Hello World!");
+
+	//Save doc file.
+	document->SaveToFile(outputFile.c_str(), FileFormat::Docx);
+	document->Close();
+	delete document;
+}
